922e58aa8ddd8ff87be07827eb72317be0017523
456Xander <daumalex10200@gmail.com>
2017-01-02 14:15:29 +0100

    Kunde Klasse gerichtet

--- src/pizza/user/Kunde.java
+++ src/pizza/user/Kunde.java
@@ -1,24 +1,45 @@
 package pizza.user;
 
-import java.awt.Component;
+public class Kunde{
 
-public class Kunde extends Login{
-
-	private int nummer;
-	private String vname;
-	private String nname;
-	private String adresse;
-	private int plz;
-	private String ort;
+	private final int nummer;
+	private final String vname;
+	private final String nname;
+	private final String adresse;
+	private final int plz;
+	private final String ort;
 	
-	public Kunde(int n){
-		nummer = n;
+	public Kunde(int nummer, String vname, String nname, String adresse, int plz, String ort){
+		this.nummer = nummer;
+		this.vname = vname;
+		this.nname = nname;
+		this.adresse = adresse;
+		this.plz = plz;
+		this.ort = ort;
 	}
-	
-	public Kunde getKunde(){
-		
-		
-		return null;
+
+	public int getNummer() {
+		return nummer;
+	}
+
+	public String getVname() {
+		return vname;
+	}
+
+	public String getNname() {
+		return nname;
+	}
+
+	public String getAdresse() {
+		return adresse;
+	}
+
+	public int getPlz() {
+		return plz;
+	}
+
+	public String getOrt() {
+		return ort;
 	}
 
 }
