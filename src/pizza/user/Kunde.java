9a9da32fa232e2cea8ba897b30eb4e0ea6b3321a
Steph161 <stephan.kramberger@sbg.at>
2017-01-02 14:56:12 +0100

    toString, Value.Of --> Kunde

--- src/pizza/user/Kunde.java
+++ src/pizza/user/Kunde.java
@@ -1,21 +1,26 @@
 package pizza.user;
-
+/*
+ * String rep: 
+ * Nummer, VName, NName, add, ort, plz
+ */
 public class Kunde{
 
 	private final String nummer;
 	private final String vname;
 	private final String nname;
 	private final String adresse;
-	private final int plz;
 	private final String ort;
+	private final int plz;
 	
-	public Kunde(String nummer, String vname, String nname, String adresse, int plz, String ort){
+	
+	public Kunde(String nummer, String vname, String nname, String adresse, String ort, int plz){
 		this.nummer = nummer;
 		this.vname = vname;
 		this.nname = nname;
 		this.adresse = adresse;
-		this.plz = plz;
 		this.ort = ort;
+		this.plz = plz;
+		
 	}
 
 	public String getNummer() {
@@ -41,5 +46,19 @@ public class Kunde{
 	public String getOrt() {
 		return ort;
 	}
-
+	
+	@Override
+	public String toString() {
+		
+		return String.format("%s, %s, %s, %s, %s, %d", nummer, vname, nname, adresse, ort, plz);
+	}
+	
+	public static Kunde valueOf(String s){
+		String parts[];
+		s = s.replace(" ", "");				//Leerzeichen entfernen
+		parts = s.split(",");
+		int plzn = Integer.parseInt(parts[5]);
+		Kunde ret = new Kunde(parts[0], parts[1], parts[2], parts[3], parts[4], plzn);
+		return ret;
+	}
 }
