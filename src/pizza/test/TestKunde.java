534e5413faa1057ee014151c6d4844bda49026e4
456Xander <daumalex10200@gmail.com>
2017-01-02 15:22:19 +0100

    Added test class

---
+++ src/pizza/test/TestKunde.java
@@ -0,0 +1,26 @@
+package pizza.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import pizza.user.Kunde;
+
+public class TestKunde {
+
+	@Test
+	public void testFileWrite() {
+		Kunde k = new Kunde("123", "Hans", "Moser", "Bahnhofsstraße 17", "Straßwalchen", 5204);
+		String correct = "123, Hans, Moser, Bahnhofsstraße 17, Straßwalchen, 5204";
+		String toS = k.toString();
+		assertEquals(correct, toS);
+	}
+
+	@Test
+	public void testFileRead() {
+		String orig = "123, Hans, Moser, Bahnhofsstraße 17, Straßwalchen, 5204";
+		Kunde correct = new Kunde("123", "Hans", "Moser", "Bahnhofsstraße 17", "Straßwalchen", 5204);
+		Kunde valOf = Kunde.valueOf(orig);
+		assertEquals(correct, valOf);
+	}
+}
--- src/pizza/user/Kunde.java
+++ src/pizza/user/Kunde.java
@@ -1,9 +1,10 @@
 package pizza.user;
+
 /*
  * String rep: 
  * Nummer, VName, NName, add, ort, plz
  */
-public class Kunde{
+public class Kunde {
 
 	private final String nummer;
 	private final String vname;
@@ -11,16 +12,25 @@ public class Kunde{
 	private final String adresse;
 	private final String ort;
 	private final int plz;
-	
-	
-	public Kunde(String nummer, String vname, String nname, String adresse, String ort, int plz){
+
+	/**
+	 * Creates a new Kunde object with specified data
+	 * 
+	 * @param nummer
+	 * @param vname
+	 * @param nname
+	 * @param adresse
+	 * @param ort
+	 * @param plz
+	 */
+	public Kunde(String nummer, String vname, String nname, String adresse, String ort, int plz) {
 		this.nummer = nummer;
 		this.vname = vname;
 		this.nname = nname;
 		this.adresse = adresse;
 		this.ort = ort;
 		this.plz = plz;
-		
+
 	}
 
 	public String getNummer() {
@@ -46,16 +56,24 @@ public class Kunde{
 	public String getOrt() {
 		return ort;
 	}
-	
+
 	@Override
 	public String toString() {
-		
+
 		return String.format("%s, %s, %s, %s, %s, %d", nummer, vname, nname, adresse, ort, plz);
 	}
-	
-	public static Kunde valueOf(String s){
+
+	/**
+	 * Pares a String to a Kunde, the String has to be in the following format:
+	 * <br>
+	 * Nummer, VName, NName, add, ort, plz
+	 * 
+	 * @param s
+	 * @return
+	 */
+	public static Kunde valueOf(String s) {
 		String parts[];
-		s = s.replace(" ", "");				//Leerzeichen entfernen
+		s = s.replace(" ", ""); // Leerzeichen entfernen
 		parts = s.split(",");
 		int plzn = Integer.parseInt(parts[5]);
 		Kunde ret = new Kunde(parts[0], parts[1], parts[2], parts[3], parts[4], plzn);
