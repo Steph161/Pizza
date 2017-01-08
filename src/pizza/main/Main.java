e26358c258c2ecebe3fb4c6ab7c2c562e28d8ea3
456Xander <daumalex10200@gmail.com>
2017-01-08 15:34:46 +0100

    Removed unused Libraries

--- src/pizza/main/Main.java
+++ src/pizza/main/Main.java
@@ -1,10 +1,34 @@
 package pizza.main;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import pizza.user.Kunde;
+import pizza.user.Login;
+import pizza.user.Register;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Login login = new Login();
+		login.setVisible(true);
+		Kunde k = login.doLogin();
+		login.dispose();
+		if(k == null){
+			//Register
+			Register r = new Register(login.getNummer());
+			r.setVisible(true);
+			k = r.getKundeAndClose();
+			try(BufferedWriter br = new BufferedWriter(new FileWriter("pizza.txt",true))){
+				br.write(k.toString());
+				br.write(System.lineSeparator());
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
 	}
 
 }
--- src/pizza/user/Login.java
+++ src/pizza/user/Login.java
@@ -145,4 +145,8 @@ public class Login extends JFrame {
 		return null;
 	}
 
+	public String getNummer() {
+		return nummer;
+	}
+
 }
